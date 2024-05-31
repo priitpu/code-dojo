ab0650a2cbccfe8bb164882fe8425ab139b16a28
Priit Puru <priit.puru@twn.ee>
2024-05-31 13:14:07 +0300

    .

---
+++ custom-components/brutal-slider.js
@@ -0,0 +1,37 @@
+class BrutalSlider extends HTMLElement {
+    constructor() {
+        super();
+    }
+    fetchFolders = async () => {
+        const res = await fetch(this.getAttribute('src'));
+        return await res.json();
+    };
+    async connectedCallback() {
+        const folders = await this.fetchFolders();
+        const shadow = this.attachShadow({ mode: 'open' });
+        const styles = `
+        .card {
+          box-shadow: 6px 6px 0px 3px rgb(0, 0, 0);
+          width: 100svw;
+          height: 100svh;
+          display: flex;
+          background-color: #fdfd96;
+          border: 3px solid #000;
+          border-radius: 5px;
+          position: relative;
+        }`;
+        folders.forEach((folder) => {
+            const card = document.createElement('div');
+            const iframe = document.createElement('iframe');
+            iframe.src = folder;
+            iframe.width = '100%';
+            iframe.height = '100%';
+            card.classList.add('card');
+            card.appendChild(iframe);
+            shadow.appendChild(card);
+        });
+        shadow.appendChild(createStyles(styles));
+    }
+}
+
+customElements.define('brutal-slider', BrutalSlider);
