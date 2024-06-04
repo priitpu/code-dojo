a94920d7fe0b196eba7fe971df691ebad7e93c67
Priit Puru <priit.puru@twn.ee>
2024-06-04 14:24:50 +0300

    .

--- custom-components/brutal-slider.js
+++ custom-components/brutal-slider.js
@@ -10,19 +10,25 @@ class BrutalSlider extends HTMLElement {
         const res = await fetch(this.getAttribute('src'));
         return await res.json();
     };
-    async connectedCallback() {
+    createContainer = () => {
         const shadow = this.attachShadow({ mode: 'open' });
-        const folders = await this.fetchFolders();
-        const cardStyles = `
-        .card {
-          width: 100%;
-          height: 100%;
-          display: flex;
-          background-color: #fdfd96;
-          margin: 0 auto;
-          position: relative;
-          flex-shrink: 0;
-        }`;
+        const container = document.createElement('div');
+        container.className = 'container';
+        return container;
+    };
+    createCard = () => {
+        const shadow = this.attachShadow({ mode: 'open' });
+        const card = document.createElement('div');
+        const iframe = document.createElement('iframe');
+        iframe.src = folder;
+        iframe.width = '100%';
+        iframe.height = '100%';
+        card.classList.add('card');
+        card.appendChild(iframe);
+        return card;
+    };
+    createSlider = () => {
+        const shadow = this.attachShadow({ mode: 'open' });
         const sliderStyles = `
         .slider {
             display: flex;
@@ -31,11 +37,21 @@ class BrutalSlider extends HTMLElement {
             box-shadow: 6px 6px 0px 3px rgb(0, 0, 0);
             border: 3px solid #000;
             border-radius: 5px;
+            transition: transform 0.3s ease-in-out;
         }
         .slider-scroll {
             overflow: hidden;
         }
         `;
+        const slider = document.createElement('div');
+        slider.className = 'slider';
+        const sliderScroll = document.createElement('div');
+        sliderScroll.className = 'slider-scroll';
+        shadow.appendChild(createStyles(sliderStyles));
+        return [slider, sliderScroll];
+    };
+    createButtons = () => {
+        const shadow = this.attachShadow({ mode: 'open' });
         const [leftButton, rightButton] = [document.createElement('button'), document.createElement('button')];
         const buttonStyles = `
             .button {
@@ -50,6 +66,14 @@ class BrutalSlider extends HTMLElement {
         rightButton.className = 'button';
         leftButton.style.backgroundColor = getRandomColor();
         rightButton.style.backgroundColor = getRandomColor();
+        shadow.appendChild(createStyles(buttonStyles));
+        return [leftButton, rightButton];
+    };
+    async connectedCallback() {
+        const shadow = this.attachShadow({ mode: 'open' });
+        const folders = await this.fetchFolders();
+
+
         rightButton.addEventListener('click', () => {
             this.current + 1 <= shadow.querySelectorAll('.card').length - 1 ? this.current++ : this.current = 0;
             let size = 0;
@@ -66,23 +90,24 @@ class BrutalSlider extends HTMLElement {
             }
             slider.style.transform = `translateX(${-size}px)`;
         });
-        const container = document.createElement('div');
-        container.className = 'container';
-        const slider = document.createElement('div');
-        slider.className = 'slider';
-        const sliderScroll = document.createElement('div');
-        sliderScroll.className = 'slider-scroll';
+        const container = this.createContainer();
+        const [slider, sliderScroll] = this.createSlider();
+        const [leftButton, rightButton] = this.createButtons();
         folders.forEach((folder) => {
-            const card = document.createElement('div');
-            const iframe = document.createElement('iframe');
-            iframe.src = folder;
-            iframe.width = '100%';
-            iframe.height = '100%';
-            card.classList.add('card');
-            card.appendChild(iframe);
+            const card = this.createCard(folder);
             slider.appendChild(card);
+
         });
-        shadow.appendChild(createStyles(sliderStyles));
+        const cardStyles = `
+        .card {
+          width: 100%;
+          height: 100%;
+          display: flex;
+          background-color: #fdfd96;
+          margin: 0 auto;
+          position: relative;
+          flex-shrink: 0;
+        }`;
         shadow.appendChild(createStyles(cardStyles));
         shadow.appendChild(container);
         container.appendChild(leftButton);
